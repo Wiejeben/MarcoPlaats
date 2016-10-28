9dce248cad009bfde49e969ed65caacf017373fa
Maarten de Graaf <maarten@webtraders.nl>
2016-10-28 15:09:47 +0200

    Implement base front-end webshop

--- Frontend/resources/assets/js/bootstrap.js
+++ Frontend/resources/assets/js/bootstrap.js
@@ -2,6 +2,7 @@ window._ = require('lodash');
 
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
+require('./jquery');
 
 window.Vue = require('vue');
 require('vue-resource');

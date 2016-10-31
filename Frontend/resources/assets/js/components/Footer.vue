e1d7b4bb6cbbaba7c4fcf04666f819dfadad8245
Maarten de Graaf <maarten@webtraders.nl>
2016-10-31 09:52:36 +0100

    Implement header and footer components

---
+++ Frontend/resources/assets/js/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer id="footer"><!--Footer-->
+
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row">
+                    <p class="pull-left">Copyright © 2016 HRO INF2A project 2 Group 2. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+
+    </footer><!--/Footer-->
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component ready.')
+        }
+    }
+</script>
---
+++ Frontend/resources/assets/js/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+    <div id="header">
+
+        <div class="header_top"><!--header_top-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="contactinfo">
+                            <ul class="nav nav-pills">
+                                <li><a href=""><i class="fa fa-envelope"></i> info@marcoplaats.nl</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="social-icons pull-right">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                                <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/header_top-->
+
+        <div class="header-middle"><!--header-middle-->
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="logo pull-left">
+                            <a href="index.html"><img src="images/home/logo.png" alt="" /></a>
+                        </div>
+                        <div class="btn-group pull-right">
+                            <div class="search_box pull-right">
+                                <input type="search" placeholder="Search"/>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-8">
+                        <div class="shop-menu pull-right">
+                            <ul class="nav navbar-nav">
+                                <li><a href="#"><i class="fa fa-user"></i> Account</a></li>
+                                <li><a href="#"><i class="fa fa-star"></i> Wishlist</a></li>
+                                <li><a href="#"><i class="fa fa-shopping-cart"></i> Cart</a></li>
+                                <li><a href="#"><i class="fa fa-lock"></i> Login</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!--/header-middle-->
+
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component ready.')
+        }
+    }
+</script>

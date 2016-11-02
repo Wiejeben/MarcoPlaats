84df2fc99097887573435d77ee63e3128054504b
rick droogh <rak_keeper@hotmail.com>
2016-11-02 16:11:25 +0100

    added profile page, navigation sidebar and some other pages

--- Frontend/resources/assets/js/app.js
+++ Frontend/resources/assets/js/app.js
@@ -4,7 +4,11 @@ Vue.component('header-component', require('./components/Header.vue'));
 Vue.component('footer-component', require('./components/Footer.vue'));
 Vue.component('sidebar-component', require('./components/Sidebar.vue'));
 Vue.component('products-component', require('./components/Products.vue'));
+
+Vue.component('account-sidebar-component', require('./components/account/Sidebar.vue'));
+Vue.component('account-profile-component', require('./components/account/Profile.vue'));
 Vue.component('account-settings-component', require('./components/account/Settings.vue'));
+
 Vue.component('cart-overview-component', require('./components/cart/Overview.vue'));
 Vue.component('cart-details-component', require('./components/cart/Details.vue'));
 

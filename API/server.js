4292e85bb3f595a69bbd5a4b1c7d26c7b2ac699e
Maarten de Graaf <maarten@webtraders.nl>
2016-11-02 14:10:50 +0100

    Add Google Oauth

--- API/server.js
+++ API/server.js
@@ -1,5 +1,7 @@
-var restify = require('restify');
-var MongoClient = require('mongodb').MongoClient;
+var restify = require('restify'),
+    MongoClient = require('mongodb').MongoClient,
+    passport = require('passport-restify'),
+    GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var AutoLoader = require('./Helpers/AutoLoader');
 var RestRouter = require('./Helpers/RestRouter');
@@ -14,8 +16,49 @@ var server = restify.createServer({
 });
 
 server.use(restify.fullResponse())
-      .use(restify.bodyParser());
-      
+      .use(restify.bodyParser())
+      .use(restify.queryParser())
+      .use(passport.initialize());
+
+passport.use(new GoogleStrategy({
+        clientID: "410268495427-8i08t1h9qas8n13c1ngk9oeo8vfr2fb7.apps.googleusercontent.com",
+        clientSecret: "tL7PbeaGz6z-FupnCP4PIZ1h",
+        callbackURL: "http://localhost:8080/auth/google/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        console.log('create user %s', JSON.stringify(profile));
+
+        var user = {
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            token: "he3dj320je3",
+            googleId: profile.id
+        };
+
+        return done(null, user);
+    }
+));
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+
+server.get('/auth', passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/userinfo.email'] }));
+
+server.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/failure', session: false }),
+    function(req, res) {
+
+        res.statusCode = 302;
+        res.setHeader('Location', 'http://marcoplaats.maarten.co.uk?token=' + req.user.token);
+        res.setHeader('Content-Length', '0');
+        res.end();
+    });
+
 var controllers = AutoLoader('Controllers');
 
 
@@ -35,7 +78,7 @@ MongoClient.connect(Config.Database.Url, { promiseLibrary: Promise }, function(e
 
 
 server.get('/', function (req, res, next){
-    res.send('Hello World!');
+    res.send('NodeJS is up and running!');
 });
 
 server.listen(8080, function() {

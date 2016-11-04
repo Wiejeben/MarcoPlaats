9f43416d42f55026646d3648c93c3cb1edfe00f6
Maarten de Graaf <maarten@webtraders.nl>
2016-11-04 13:49:49 +0100

    Implement base for admin pages

---
+++ Frontend/resources/assets/js/components/admin/Sidebar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="left-sidebar">
+        <h2>Menu</h2>
+        <div class="panel-group category-products" id="accordian"><!--category-productsr-->
+            <div class="panel panel-default" v-for="item in menu">
+                <div class="panel-heading">
+                    <h4 class="panel-title" v-bind:class="{ 'category-active': item.active }"><a :href="baseUrl+item.url">{{ item.name }}</a></h4>
+                </div>
+            </div>
+        </div><!--/category-productsr-->
+    </div>
+</template>
+
+<script>
+    export default {
+        mixins: [require('./../../mixins/auth')],
+
+        data() {
+            return {
+                baseUrl: '/account',
+                url: '/' + window.location.pathname.split('/').pop(),
+                menu: [
+                    { name: 'Gebruikers', url: '/admin/users', active: false },
+                    { name: 'Producten', url: '/admin/products.html', active: false },
+                    { name: 'Instellingen', url: '/admin/settings', active: false }
+                ],
+            }
+        },
+        created() {
+            var self = this;
+            self.menu.forEach(function(item) {
+                // console.log(self.url);
+                item.active = (item.url == self.url);
+            });
+        }
+    }
+</script>
---
+++ Frontend/resources/assets/js/components/admin/Users.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="items"><!--features_items-->
+        <h2 class="title text-center">Beheer</h2>
+        <h3>Gebruikers</h3>
+    </div>
+</template>
+<script>
+    export default {
+        created() {
+            console.log('Users admin ready.');
+        }
+    }
+</script>

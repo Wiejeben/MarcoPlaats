98eb625a38087cfe7a01a565a8db0be0cce19f0e
Jacky Fong <J.wfong@hotmail.com>
2016-11-07 16:31:26 +0100

    Added user profile

--- Frontend/resources/assets/js/components/account/Profile.vue
+++ Frontend/resources/assets/js/components/account/Profile.vue
@@ -2,14 +2,39 @@
     <div class="items"><!--features_items-->
         <h2 class="title text-center">Instellingen</h2>
         <h3>Account</h3>
+        <div v-if="user != null">
+            <dl class="dl-horizontal">
+                <dt>Naam:</dt>
+                <dd>{{ user.FirstName }} {{ user.LastName }}</dd>
+                <dt>Email:</dt>
+                <dd>{{ user.Email }}</dd>
+                <dt>Telefoon:</dt>
+                <dd>{{ user.PhoneNumber }}</dd>
+                <dt>Adres:</dt>
+                <dd>{{ user.MainAddress.Address }}</dd>
+                <dt>City:</dt>
+                <dd>{{ user.MainAddress.City }}</dd>
+                <dt>Postcode:</dt>
+                <dd>{{ user.MainAddress.Zipcode }}</dd>
+            </dl>
+        </div>
     </div>
 </template>
 <script>
     export default {
+        mixins: [require('./../../mixins/auth')],
         created() {
-            HasRole('user')
+            var self = this;
+            HasRole('user', function(){
+                self.user = window.User;
+            })
 
             console.info('created');
+        },
+        data() {
+            return {
+                user: null
+            }
         }
     }
 </script>

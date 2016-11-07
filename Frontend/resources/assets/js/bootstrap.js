91d5d3cd69d83f0d154ea59b4376578ec41714fa
rick droogh <rak_keeper@hotmail.com>
2016-11-07 14:05:12 +0100

    deleted code from daan!

--- Frontend/resources/assets/js/bootstrap.js
+++ Frontend/resources/assets/js/bootstrap.js
@@ -16,11 +16,15 @@ window.User = null;
 // Generate API url
 var location = window.location.hostname;
 
+if(location == 'localhost'){
+    localStorage.setItem('apiUrl', '146.185.176.116');
+}
+
 if (localStorage.getItem('apiUrl') != null) { // You may overwrite the API url by defining a different one in local storage
     location = localStorage.getItem('apiUrl');
 }
 
-window.apiUrl = 'http://' + 'localhost' + ':8080';
+window.apiUrl = 'http://' + location + ':8080';
 
 window.LoggedIn = false;
 

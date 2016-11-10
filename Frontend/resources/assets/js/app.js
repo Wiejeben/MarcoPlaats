1d9e60abc7b67e4baa4f2ea136ab33c7083632fd
Maarten de Graaf <wiejeben@gmail.com>
2016-11-10 11:09:46 +0100

    Implement login feedback

--- Frontend/resources/assets/js/app.js
+++ Frontend/resources/assets/js/app.js
@@ -11,13 +11,16 @@ const app = new Vue({
             $.get(window.apiUrl + '/auth/user', function(data) {
                 if(data && JSON.stringify(data) != '{}')
                 {
-                    if(data.Role != 'blocked'){
-                        window.User = data;
-                        eventHub.$emit('user-detected', data);
-                    }else{
+                    if(data.Role == 'blocked')
+                    {
                         window.LoggedIn = false;
                         localStorage.removeItem('authorization');
-                        window.location.href = "/";
+                        window.location.href = "/?feedback=blocked";
+                    }
+                    else
+                    {
+                        window.User = data;
+                        eventHub.$emit('user-detected', data);
                     }
                 }
                 else
@@ -30,3 +33,28 @@ const app = new Vue({
         }
     }
 });
+
+var feedback = location.search.split('feedback=')[1];
+
+if (typeof feedback != "undefined") {
+
+    if (feedback == 'blocked')
+    {
+        eventHub.$emit('show-alert', { Type: 'danger', Message: 'Uw account is geblokkeerd.' });
+    }
+
+    if (feedback == 'token_undefined')
+    {
+        eventHub.$emit('show-alert', { Type: 'warning', Message: 'Er is helaas iets is gegaan tijdens het inlogproces met Google, probeer het opnieuw.' });
+    }
+
+    if (feedback == 'localstorage')
+    {
+        eventHub.$emit('show-alert', { Type: 'warning', Message: 'Uw browser ondersteunt geen localStorage, hierdoor kan uw login sessie niet opgeslagen worden.' })
+    }
+
+    if (feedback == 'unknown_failure')
+    {
+        eventHub.$emit('show-alert', { Type: 'danger', Message: 'Uw login verzoek kan op dit moment helaas niet verwerkt worden.' })
+    }
+}

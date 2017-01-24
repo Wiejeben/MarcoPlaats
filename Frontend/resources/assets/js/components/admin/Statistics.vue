c39831ee129bb9675ff551fc11b986d458ae3652
rick droogh <rak_keeper@hotmail.com>
2017-01-24 16:15:23 +0100

    added product order chart

--- Frontend/resources/assets/js/components/admin/Statistics.vue
+++ Frontend/resources/assets/js/components/admin/Statistics.vue
@@ -2,7 +2,12 @@
     <div class="items"><!--features_items-->
         <h2 class="title text-center">Beheer</h2>
         <h3>Statistieken</h3>
-        <vue-chart v-if="charts.categories.ajaxLoaded" type="radar" ref="categories" :data="charts.categories"></vue-chart>
+        <vue-chart v-if="charts.categories.ajaxLoaded" type="radar" ref="categories" :data="charts.categories" :option="charts.Options"></vue-chart>
+        <div v-else class="text-center">
+            <img src="/images/loading.gif" alt="">
+        </div>
+        <br><br>
+        <vue-chart v-if="charts.productOrder.ajaxLoaded" type="bar" ref="productOrder" :data="charts.productOrder" :option="charts.Options"></vue-chart>
         <div v-else class="text-center">
             <img src="/images/loading.gif" alt="">
         </div>
@@ -24,6 +29,15 @@ import VueChart from 'vue-chart';
                 });
                 ChartData.ajaxLoaded = true;
             });
+            $.get(apiUrl + '/statistics/productordercount', function(data) {
+                var ChartData = self.$data.charts.productOrder;
+                data.forEach(function(category) {
+                    ChartData.labels.push(category._id.Name)
+                    ChartData.datasets[0].backgroundColor.push(self.randomColor())
+                    ChartData.datasets[0].data.push(category.Amount);
+                });
+                ChartData.ajaxLoaded = true;
+            });
         },
         components: {
             VueChart
@@ -31,6 +45,16 @@ import VueChart from 'vue-chart';
         data() {
             return {
                charts: {
+                    Options: {
+                        scales: {
+                            yAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'probability'
+                                }
+                            }]
+                        }
+                    },
                     categories: {
                         ajaxLoaded: false,
                         labels: [],
@@ -59,14 +83,18 @@ import VueChart from 'vue-chart';
                             }
                         ]
                     },
-                    test: {
-                        labels: ["September", "Oktober", "November", "December", "Januari"],
-                        datasets: [{
-                            backgroundColor: ["#36A2EB", "#FF6384"],
-                            data: [1, 1, 5, 10, 15]
-                        }]
+                    productOrder: {
+                        ajaxLoaded: false,
+                        labels: [],
+                        datasets: [
+                            {
+                                label: "Aantal verkochte producten",
+                                backgroundColor: [],
+                                data: []
+                            }
+                        ]
                     }
-               }
+                }
             }
         },
         methods:{

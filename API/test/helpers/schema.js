8150bc855c3b2a79ac4d63d0cf1dd8b6caa2911b
Jacky Fong <J.wfong@hotmail.com>
2017-01-25 11:36:20 +0100

    test for sanitize

---
+++ API/test/helpers/schema.js
@@ -0,0 +1,11 @@
+module.exports = {
+    Name: null,
+    Description: null,
+    Price: null,
+    Amount: null,
+    Images: [],
+    SellerID: null,
+    CreatedAt: null,
+    DeletedAt: null,
+    DeliveryMethod: null
+};
--- API/test/unit.js
+++ API/test/unit.js
@@ -1,5 +1,8 @@
 const test = require('unit.js');
 var BaseModel = require("./../Helpers/BaseModel");
+var Model = require("./../Helpers/Model");
+
+var schema = require("./helpers/schema");
 
 // Define globals
 global.db = require('./helpers/db');
@@ -10,11 +13,38 @@ describe('Unit tests', () => {
 
     it('Check ObjectId validation', () => {
         test.must(new BaseModel("mock").validateId("this_is_a_fake_objectid")).be.false();
-        test.must(new BaseModel("mock").validateId("588863a05c23f611e97ce9b21")).be.true();
+        test.must(new BaseModel("mock").validateId("588863a05c23f611e97ce9b2")).be.true();
     });
 
     it('Test sanitizer', () => {
-        // Test if sanitizer does its job right
+        var model = new Model("mock", schema);
+
+        model.document = {
+            Name: "Bike",
+            Description: "New bike",
+            Price: 300,
+            Amount: 1,
+            SellerID: "58208d29f62ed51b5c78f2a1",
+            CreatedAt: null,
+            DeletedAt: null,
+            DeliveryMethod: null
+        }
+
+        model.sanitize();
+
+        var expected = {
+            Name: "Bike",
+            Description: "New bike",
+            Price: 300,
+            Amount: 1,
+            Images: [],
+            SellerID: "58208d29f62ed51b5c78f2a1",
+            CreatedAt: null,
+            DeletedAt: null,
+            DeliveryMethod: null
+        }
+
+        test.object(model.document).match(expected);
     });
 
 });

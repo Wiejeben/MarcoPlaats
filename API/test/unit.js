64ddab02a2c0fc6ebcc9b27379fcca77aba3a465
Maarten de Graaf <wiejeben@gmail.com>
2017-01-25 11:02:56 +0100

    Implement ObjectID unit test

--- API/test/unit.js
+++ API/test/unit.js
@@ -1,13 +1,16 @@
 const test = require('unit.js');
 var BaseModel = require("./../Helpers/BaseModel");
 
-require('./../server')
+// Define globals
+global.db = require('./helpers/db');
+global.ObjectId = require('mongodb').ObjectId;
 
 // http://unitjs.com/guide/quickstart.html
 describe('Unit tests', () => {
 
     it('Check ObjectId validation', () => {
-        //test.must(BaseModel.validateId("balbalbalbla")).be.false();
+        test.must(new BaseModel("mock").validateId("this_is_a_fake_objectid")).be.false();
+        test.must(new BaseModel("mock").validateId("588863a05c23f611e97ce9b21")).be.true();
     });
 
     it('Test sanitizer', () => {

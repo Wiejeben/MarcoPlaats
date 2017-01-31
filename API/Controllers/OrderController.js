d93c27fd1fb7fc3b054f303eae95c00ecdc9bab5
Jacky Fong <J.wfong@hotmail.com>
2017-01-31 11:12:54 +0100

    Added admin orders overview

--- API/Controllers/OrderController.js
+++ API/Controllers/OrderController.js
@@ -1,8 +1,20 @@
 const RestfulController = require('./../Helpers/RestfulController'),
-    OrderModel = require('./../Models/Order');
+    Model = require('./../Models/Order');
 
 module.exports = class OrderController extends RestfulController {
     constructor(req, res, next) {
-        super(OrderModel, req, res, next)
+        super(Model, req, res, next)
     }
+
+    index() {
+        this.model.getAllOrders()
+        .then(result => {
+            console.log(result)
+            this.res.send(result)
+        })
+        .catch(this.next)
+    }  
+    
+
+
 };
